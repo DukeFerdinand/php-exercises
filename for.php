4aa939a364c3dec43b8c16a15fcb3057b723ba93
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-20 11:52:13 -0500

    Added cowsay array

--- for.php
+++ for.php
@@ -1,4 +1,11 @@
 <?php
+	$str = "apt, beavis.zen, bong, bud-frogs, bunny, calvin, cheese, cock, cower, daemon, default, dragon, dragon-and-cow, duck, elephant, elephant-in-snake, eyes, flaming-sheep, ghostbusters, gnu, head-in, hellokitty, kiss, kitty, koala, kosh, luke-koala, mech-and-cow, meow, milk, moofasa, moose, mutilated, pony, pony-smaller, ren, sheep, skeleton, snowman, sodomized-sheep, stegosaurus, stimpy, suse, three-eyes, turkey, turtle, tux, unipony, unipony-smaller, vader, vader-koala, www";
+	
+
+	$arr = explode(", ", $str);
+ 	// var_dump($arr);
+ 	// echo `cowsay -f "$arr[$rand]" mooo`;
+
 	fwrite(STDOUT, "Starting Number: ");
 	$start = trim(fgets(STDIN));
 
@@ -24,13 +31,20 @@
 
 	for($i = $start; $i <= $end; $i += $inc){
 		if ($i % 3 == 0 && $i % 5 == 0 ){
-			echo "FizzBuzz" . PHP_EOL;
+			$rand = mt_rand(0, 51);
+			fwrite(STDOUT, `cowsay -f "$arr[$rand]" FizzBuzz`);
 		} else if ($i % 3 == 0) {
-			echo "Fizz" . PHP_EOL;
+			$rand = mt_rand(0, 51);
+			// echo "Fizz" . PHP_EOL;
+			fwrite(STDOUT, `cowsay -f "$arr[$rand]" Fizz`);
 		} else if ($i % 5 == 0) {
-			echo "Buzz" . PHP_EOL;
+			$rand = mt_rand(0, 51);
+			// echo "Buzz" . PHP_EOL;
+			fwrite(STDOUT, `cowsay -t "$arr[$rand]" Buzz`);
 		} else {
 			echo $i . PHP_EOL;
 		}
 	}
+
+
 ?>
